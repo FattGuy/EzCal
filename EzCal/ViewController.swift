d539b137d2d85481ba7fb04d62a09625863e9869
FattGuy <chang809@umn.edu>
2017-08-23 21:49:03 -0500

    First commit

---
+++ EzCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EzCal
+//
+//  Created by Feng Chang on 17/8/23.
+//  Copyright © 2017年 FattGuy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
